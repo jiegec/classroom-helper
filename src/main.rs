535492336f9646ad0dcc63d782e5ffeaac85bc57
Jiajie Chen <noc@jiegec.ac.cn>
2019-07-02 15:32:06 +0800

    Silence git output

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate clap;
 
 use clap::{App, Arg};
 use std::fs::File;
-use std::process::Command;
+use std::process::{Command, Stdio};
 
 fn main() {
     let args = App::new("classrom-helper")
@@ -16,21 +16,24 @@ fn main() {
                 .value_name("org")
                 .help("GitHub organization name, e.g. physics-data")
                 .takes_value(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("prefix")
                 .short("p")
                 .long("prefix")
                 .value_name("prefix")
                 .help("GitHub repo prefix, e.g. self-intro")
                 .takes_value(true),
-        ).arg(
+        )
+        .arg(
             Arg::with_name("students")
                 .short("s")
                 .long("students")
                 .value_name("students")
                 .help("Path to students csv, e.g. students.csv")
                 .takes_value(true),
-        ).get_matches();
+        )
+        .get_matches();
 
     let org = args.value_of("org").unwrap_or("physics-data");
     let prefix = args.value_of("prefix").unwrap_or("self-intro");
@@ -40,17 +43,22 @@ fn main() {
     for row in rdr.records() {
         let record = row.unwrap();
         let github = record.get(2).unwrap();
-        println!("{:?} {:?}", record, github);
         let output = Command::new("git")
             .current_dir("workspace")
             .arg("clone")
             .arg(format!("git@github.com:{}/{}-{}.git", org, prefix, github))
-            .status().unwrap();
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()
+            .unwrap();
         println!("{:?}", output);
         let output = Command::new("git")
             .current_dir(format!("workspace/{}-{}", prefix, github))
             .arg("pull")
-            .status().unwrap();
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()
+            .unwrap();
         println!("{:?}", output);
     }
 
